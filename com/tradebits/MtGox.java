a698d11f57c9d186b0485e23d4a84afdca8ae72c
Adam Wulf <adam.wulf@gmail.com>
2012-08-10 03:22:29 -0500

    initial commit of java bot

---
+++ com/tradebits/MtGox.java
@@ -0,0 +1,52 @@
+package com.tradebits;
+
+import java.net.*;
+import java.util.*;
+import com.kaazing.gateway.client.html5.WebSocket;
+import com.kaazing.gateway.client.html5.WebSocketAdapter;
+import com.kaazing.gateway.client.html5.WebSocketEvent;
+import org.java_websocket.client.*;
+import org.java_websocket.drafts.*;
+import org.java_websocket.handshake.*;
+import java.nio.ByteBuffer;
+
+
+public class MtGox {
+    
+    WebSocketClient socket;
+    
+    public MtGox(){
+        try{
+            String wsURL = "ws://websocket.mtgox.com/mtgox";
+            socket = new WebSocketClient(new URI(wsURL), new Draft_76()){
+                @Override
+                public void onOpen( ServerHandshake handshakedata ){
+                    System.out.println("onOpen");
+                }
+                @Override
+                public void onMessage( String message ){
+                    System.out.println("onMessage: " + message);
+                }
+                @Override
+                public void onClose( int code, String reason, boolean remote ){
+                    System.out.println("onClose");
+                }
+                @Override
+                public void onError( Exception ex ){
+                    System.out.println("onError");
+                    ex.printStackTrace();
+                }
+                @Override
+                public void onMessage( ByteBuffer bytes ) {
+                    System.out.println("onMessage");
+                }
+            };
+        }catch(Exception e){
+        }
+    }
+    
+    public void connect(){
+        socket.connect();
+    }
+    
+}
---
+++ com/tradebits/Trader.java
@@ -0,0 +1,14 @@
+package com.tradebits;
+
+
+public class Trader{
+    
+     public static void main(String[] args) throws Exception {
+         
+         MtGox mtgox = new MtGox();
+         mtgox.connect();
+         
+         
+         
+     }
+}
