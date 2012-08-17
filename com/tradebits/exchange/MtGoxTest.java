b7184250a00c6e98bbcbf302c42ee032bb8a13cc
Adam Wulf <adam.wulf@gmail.com>
2012-08-17 02:57:32 -0500

    stub test file for mtgox is ready

---
+++ com/tradebits/exchange/MtGoxTest.java
@@ -0,0 +1,13 @@
+package com.tradebits.exchange;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MtGoxTest {
+    
+    @Test
+    public void testAdd() {
+        assertEquals(5, 5);
+    }
+    
+}
